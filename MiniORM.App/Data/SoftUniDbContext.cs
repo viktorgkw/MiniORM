1eb60a347fe5d6371fd7d34eac2edf3e57f01efe
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-02-26 11:36:59 +0200

    Final update in MiniORM.App

--- MiniORM.App/Data/SoftUniDbContext.cs
+++ MiniORM.App/Data/SoftUniDbContext.cs
@@ -4,11 +4,11 @@ namespace MiniORM.App.Data;
 
 public class SoftUniDbContext : DbContext
 {
-	public SoftUniDbContext(string connectionString)
-		: base(connectionString) { }
+    public SoftUniDbContext(string connectionString)
+        : base(connectionString) { }
 
-	public DbSet<Employee> Employees { get; set; }
-	public DbSet<Project> Projects { get; set; }
-	public DbSet<Department> Departments { get; set; }
-	public DbSet<EmployeeProject> EmployeesProjects { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<EmployeeProject> EmployeesProjects { get; set; }
 }
--- MiniORM.App/StartUp.cs
+++ MiniORM.App/StartUp.cs
@@ -9,5 +9,28 @@ public class StartUp
         SoftUniDbContext dbContext = new SoftUniDbContext(Config.ConnectionString);
 
         Console.WriteLine("Connection success!");
+
+        Thread.Sleep(1000);
+
+        Console.Clear();
+
+        // Add
+
+        //Employee newEmployee = new Employee()
+        //{
+        //    FirstName = "Test",
+        //    LastName = "Testov",
+        //    DepartmentId = dbContext.Departments.First().Id,
+        //    IsEmployed = true
+        //};
+        //dbContext.Employees.Add(newEmployee);
+
+        // Delete
+
+        //Employee employee = dbContext.Employees
+        //    .First(e => e.FirstName == "Test");
+        //dbContext.Employees.Remove(employee);
+
+        dbContext.SaveChanges();
     }
 }
